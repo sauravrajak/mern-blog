2ed31d3fe9d837a218cca86f699c01bbc9f973ac
saurav kumar rajak <sauravrajak@outlook.com>
2024-01-25 12:29:38 +0530

    installed reactjs & tailwind css and created first template

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-4xl font-bold text-green-400 text-center'>
+        Hello Vite
+      </h1>
+    </div>
+  )
+}
+
+export default App
