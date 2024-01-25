143e12e7ea08909587e69b1a60373fcc723a2927
saurav kumar rajak <sauravrajak@outlook.com>
2024-01-25 14:51:07 +0530

    created pages & routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <h1 className='text-4xl font-bold text-green-400 text-center'>
-        Hello Vite
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
