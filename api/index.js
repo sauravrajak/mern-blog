ffdde0cf93053f320164a4e871f1d19fcca80dbf
saurav kumar rajak <sauravrajak@outlook.com>
2024-01-26 12:53:23 +0530

    crate database connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const app = express()
 
+const PORT = 3000
+
 app.listen(3000, () => {
-  console.log(`Server is running on port 3000!!!`)
+  console.log(`Server is running on port ${PORT}!!!`)
 })
