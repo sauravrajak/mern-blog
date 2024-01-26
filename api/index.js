692b13f9f58031016874664d68831225d9aa215d
saurav kumar rajak <sauravrajak@outlook.com>
2024-01-26 12:54:56 +0530

    added mongodb to server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express()
 
 const PORT = 3000
+
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => console.log('Db is connected'))
+  .catch((error) => console.log(error.message))
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}!!!`)
